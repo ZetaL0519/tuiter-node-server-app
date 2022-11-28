2f0f7d09ab6e76a298ff342655c1b4935e8c4777
ZoeLee <zoelee@ZoeLees-MacBook-Pro.local>
2022-11-28 08:51:30 -0800

    a9 Mongo fa22

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+  tuit: String,
+  likes: Number,
+  dislikes: Number,
+  liked: Boolean,
+}, {collection: 'tuits'});
+export default schema;
